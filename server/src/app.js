5661dd0e166f5bf339cca8e9ae80dca01973c50b
Aleksey Sizov <zapad7715@ya.ru>
2019-03-24 17:58:07 +0300

    server it's work

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,23 @@
-console.log('hello');
+// eslint-disable-next-line no-undef
+const express = require('express');
+// eslint-disable-next-line no-undef
+const bodyParser = require('body-parser');
+// eslint-disable-next-line no-undef
+const cors = require('cors');
+// eslint-disable-next-line no-undef
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req, res) => { 
+  res.send({
+    message: 'hello world'
+  });
+});
+
+app.listen(port);
